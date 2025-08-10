eced626ea94ad17d9048066779a7ec982dfa9597
yazon <czaplejewicz@gmail.com>
2025-08-10 12:53:35 +0200

    arm64/macros: fix V4SF_K_N twiddle interleave to use vzip on low lanes to build {re0,im0,re1,im1}

--- src/macros.h
+++ src/macros.h
@@ -53,18 +53,14 @@ V4SF_K_N(int inv,
      * time so both architectures build.
      */
 #ifdef HAVE_ARM64
-    /* re = {re0,re0,re1,re1},  im = {im0,im0,im1,im1}           */
-    /* Want  {re0,im0,re1,im1}                                   */
-
-    /* Interleave unique lanes: {re0,im0,re1,im1} */
+    /* re = {re0,re0,re1,re1},  im = {im0,im0,im1,im1}
+     * Build interleaved twiddle vector {re0,im0,re1,im1} from low half only.
+     */
     float32x2_t re_low  = vget_low_f32(re);   /* r0,r0 */
-    float32x2_t re_high = vget_high_f32(re);  /* r1,r1 */
     float32x2_t im_low  = vget_low_f32(im);   /* i0,i0 */
-    float32x2_t im_high = vget_high_f32(im);  /* i1,i1 */
-
-    float32x2_t tw_low  = vzip1_f32(re_low,  im_low);   /* r0,i0 */
-    float32x2_t tw_high = vzip1_f32(re_high, im_high);  /* r1,i1 */
-    V4SF tw = vcombine_f32(tw_low, tw_high);
+    float32x2_t tw_lo   = vzip1_f32(re_low, im_low);    /* {r0,i0} */
+    float32x2_t tw_hi   = vzip2_f32(re_low, im_low);    /* {r1,i1} */
+    V4SF tw = vcombine_f32(tw_lo, tw_hi);
     zk_p = V4SF_IMUL(*r2, tw);
     zk_n = V4SF_IMULJ(*r3, tw);
 #else
